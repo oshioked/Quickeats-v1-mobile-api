727b616c0610b25b55fabd709cf1d6608f04fbb2
oshioked <danieloshos3@gmail.com>
2020-07-13 02:32:22 +0100

    logging any possible error from posting data to db

--- routes/category.js
+++ routes/category.js
@@ -4,8 +4,12 @@ const database = require('../database');
 const CATEGORIES = require('../data/categories.data')
 
 router.get('/postall', async (req, res) =>{
-  const response = await database.insert(CATEGORIES).into('categories');
-  res.json('success');
+  try {
+    const response = await database.insert(CATEGORIES).into('categories');
+    res.json('success');
+  } catch (error) {
+    res.send(error)
+  }
 })
 
 router.get('/', async (req, res) => {
