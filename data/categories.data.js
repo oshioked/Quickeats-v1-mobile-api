247e590572db7ce2fe04b215a536225bef0b7897
oshioked <danieloshos3@gmail.com>
2020-07-13 06:29:28 +0100

    putting correct image urls and fix some description for categories

--- data/categories.data.js
+++ data/categories.data.js
@@ -2,25 +2,25 @@ const CATEGORIES = [
     {
       id: 'c1',
       title:  'Snaaaacks',
-      imageurl:  './images/junk-food.jpg',
-      description:  'Nice and cooool snacks both local and international'
+      imageurl:  'https://i0.wp.com/www.eatthis.com/wp-content/uploads/2018/01/junk-food.jpg?resize=640%2C360&ssl=1',
+      description:  'Healthy snacks between meals can also decrease your hunger and keep you from overeating at meal time. We have those nice unhealthy snacks too though.'
     },
     {
       id:  'c2',
       title:  'Beverages',
-      imageurl:  './images/Beverage.jpg',
-      description:  'We have a variety of soft drinks, fruit juices, hard drinks, etc. Anyway you like it.'
+      imageurl:  'https://dynaimage.cdn.cnn.com/cnn/c_fill,g_auto,w_1200,h_675,ar_16:9/https%3A%2F%2Fcdn.cnn.com%2Fcnnnext%2Fdam%2Fassets%2F160804125049-sprite-cans.jpg',
+      description:  'Beverages not only provide essential hydration but can also be a source of other nutrients. We have a variety of soft drinks, fruit juices, hard drinks, etc. Anyway you like it.'
     },
     {
       id:  'c3',
       title:  'Breakfast',
-      imageurl: './images/breakfasts.jpg',
-      description:  "Any meal made with the famous haseolus vulgaris. You're search has to end here cause akara is here."
+      imageurl: 'https://thumbor.thedailymeal.com/p3TNvWckTkIUJVl8xhs7yw4CQp4=/870x565/filters:focal(600x400:601x401)/https://www.thedailymeal.com/sites/default/files/2019/01/18/0-Utah-MAIN2.jpg',
+      description:  "You're search has to end here cause akara is here.  Meals to jumpstarts your metabolism and, thus, helps you burn more calories throughout the day."
     },
     {
       id:  'c4',
       title:  'Full Meals',
-      imageurl:  './images/Meals.jpg',
+      imageurl:  'https://previews.123rf.com/images/vladstar/vladstar1602/vladstar160200291/52176738-tasty-dish-of-chicken-thigh-with-rice-and-salad-leaves.jpg',
       description:  "Meals that will leave you full las las"
     }
   
